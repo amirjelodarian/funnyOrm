2a539cfb342cc403b788406fb92e35a495a5ddfb
amir <amirjelodarian@gmail.com>
2021-12-11 01:51:53 +0330

    c
    c

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -6,8 +6,24 @@ use App\Models\User;
 use function App\Utilities\view;
 class UserController {
     public function index(){
+           // var_dump($user->create([
+                //     "user_mode" => "standard",
+                //     "username" => "aslu",
+                //     "password" => "asli",
+                //     "tell" => '09303644573',
+                //     "email" => "asddli@gmail.com",
+                //     "address" => "asgar abad",
+                //     "first_name" => "aslu",
+                //     "last_name" => "sas",
+                //     "create_at" => strftime('%Y-%m-%d %H:%M:%S',time()),
+                //     "last_login" => strftime('%Y-%m-%d %H:%M:%S',time()),
+                //     "pro_pic" => "sa54d5.jpg",
+                //     "account_status" => "aslyy",
+                // ]));
+                // var_dump($user->users());
+                // var_dump($user2->where('username','amir')->where('AND id',10)->get());
         $user = new User2();
-        View::view('myTest',["users" => $user->all(),"DB" => $user->db]);
+        view('myTest',["users" => $user->all(),"DB" => $user->db]);
     }
     public function submit(){
         $user = new User2();
--- public/index.php
+++ public/index.php
@@ -2,24 +2,14 @@
 require_once "../vendor/autoload.php";
 use App\Models\BaseModels\Route;
 $route = new Route();
+?>
 
-       
-                // var_dump($user->create([
-                //     "user_mode" => "standard",
-                //     "username" => "aslu",
-                //     "password" => "asli",
-                //     "tell" => '09303644573',
-                //     "email" => "asddli@gmail.com",
-                //     "address" => "asgar abad",
-                //     "first_name" => "aslu",
-                //     "last_name" => "sas",
-                //     "create_at" => strftime('%Y-%m-%d %H:%M:%S',time()),
-                //     "last_login" => strftime('%Y-%m-%d %H:%M:%S',time()),
-                //     "pro_pic" => "sa54d5.jpg",
-                //     "account_status" => "aslyy",
-                // ]));
-                // var_dump($user->users());
-                // var_dump($user2->where('username','amir')->where('AND id',10)->get());
-                $route->get('a',"UserController@index");
-                $route->post('b',"UserController@submit");
-            ?>
+
+
+
+<?php 
+
+$route->get('a',"UserController@index");
+$route->post('b',"UserController@submit");
+
+?>
