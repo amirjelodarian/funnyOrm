037843db0099863a9f5d735cb06ff5467ae63104
amir <amirjelodarian@gmail.com>
2021-12-11 23:24:16 +0330

    Update index.php

--- public/index.php
+++ public/index.php
@@ -11,14 +11,27 @@ $route = new Route();
 
 <?php 
 
-$route->get('a',function(){
- echo "eee";
-});
-$route->post('b',"UserController@submit");
+// $route->get('a',function(){
+//  echo "eee";
+// });
+// $route->post('b',"UserController@submit");
 
-$route->get('createuser',function(){
-    $user = new User2();
-    View::view('myTest',["users" => $user->all(),"DB" => $user->db]);
-});
+// $route->get('createuser',function(){
+//     $user = new User2();
+//     View::view('myTest',["users" => $user->all(),"DB" => $user->db]);
+// });
+$str = "book/{bookid}/blog{blogid}";
+function getStringsBetween($str,$with_from_to=true ,$start='{', $end='}'){
+    $arr = [];
+    $last_pos = 0;
+    $last_pos = strpos($str, $start, $last_pos);
+    while ($last_pos !== false) {
+        $t = strpos($str, $end, $last_pos);
+        $arr[] = ($with_from_to ? $start : '').substr($str, $last_pos + 1, $t - $last_pos - 1).($with_from_to ? $end : '');
+        $last_pos = strpos($str, $start, $last_pos+1);
+    }
+    return $arr; 
+}
 
+echo getStringsBetween($str,false)[0];
 ?>
